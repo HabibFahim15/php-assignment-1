d6c161659316ebcb9279a40e8cdbe8be0aed7b07
Habib Hossain <habibfahim71@gmail.com>
2025-07-04 23:39:46 +0600

    positive-negative and odd even are in same file

--- number.php
+++ number.php
@@ -14,5 +14,16 @@ if($number > 0){
     echo "The number is zero. \n";
 }
 
+//number is even or odd
+
+echo "Enter a number : ";
+
+$NUMBER = (int)readline();
+
+if($NUMBER % 2 == 0){
+    echo "The number is even. \n";
+}else{
+    echo "The number is odd. \n";
+}
 
 ?>
