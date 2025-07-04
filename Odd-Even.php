2ee5bd629c41c2ebef7c2054fa2c310c295967ad
Habib Hossain <habibfahim71@gmail.com>
2025-07-04 23:36:27 +0600

    Odd-even are done

---
+++ Odd-Even.php
@@ -0,0 +1,14 @@
+<?php 
+//number is even or odd
+
+echo "Enter a number : ";
+
+$NUMBER = (int)readline();
+
+if($NUMBER % 2 == 0){
+    echo "The number is even. \n";
+}else{
+    echo "The number is odd. \n";
+}
+
+?>
--- number.php
+++ number.php
@@ -14,16 +14,5 @@ if($number > 0){
     echo "The number is zero. \n";
 }
 
-//number is even or odd
-
-echo "Enter a number : ";
-
-$NUMBER = (int)readline();
-
-if($NUMBER % 2 == 0){
-    echo "The number is even. \n";
-}else{
-    echo "The number is odd. \n";
-}
 
 ?>
